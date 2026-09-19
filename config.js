d19c3fa455321d8682ab8b173351eddb2a131db9
Duncan Langrume <soluceplay@gmail.com>
2026-09-20 01:25:06 +0200

    Latest Commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.BLOCKDIRECT_CONFIG = {
-  githubUrl: "https://github.com/SolucePlay/TON-REPO",
-  windowsDownloadUrl: "",
-  playStoreUrl: "",
+  githubUrl: "https://github.com/SolucePlay/BlockDirect",
+  windowsDownloadUrl: "https://github.com/SolucePlay/BlockDirect/releases/download/v0.9.1/Install-BlockDirect-v0.9.1.exe",
+  playStoreUrl: "https://github.com/SolucePlay/BlockDirect/releases/download/v0.9.1/BlockDirect-v0.9.1.apk",
   releaseVersion: "0.9.1",
   contactEmail: "contact@blockdirect.fr",
   securityEmail: "security@blockdirect.fr"
